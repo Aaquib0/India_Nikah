6f1dc73b5e47cd296f2eb8e7aebead394a21ec39
Tanveer Tamboli <tanveertamboli80@gmail.com>
2023-02-01 01:07:06 +0530

    flatList Selection

--- App.js
+++ App.js
@@ -1,94 +1,68 @@
-import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
-import React from 'react';
-// import Routes from './SRC/Navigation/Routes'
-import {
-  responsiveFontSize,
-  responsiveWidth,
-  responsiveHeight,
-} from 'react-native-responsive-dimensions';
+import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [data, setdata] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+
+  const onSelect = ind => {
+    const tempData = [];
+    data.map((item, index) => {
+      if (index == ind) {
+        if (item==true){
+          tempData.push(false)
+        } else{
+            tempData.push(true)
+          }
+      } else {
+        if (item==true){
+          tempData.push(true)
+        } else{
+            tempData.push(false)
+          }
+      }
+    });
+    setdata(tempData);
+  };
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <View style={{flexDirection:'row'}}>
-      <View
-        style={{
-          width: responsiveWidth(20),
-          height: responsiveWidth(20),
-          borderRadius:responsiveWidth(10),
-          backgroundColor: 'green',
-          marginLeft:responsiveWidth(5)
-        }}></View>
-        <View
-        style={{
-          width: responsiveWidth(20),
-          height: responsiveWidth(20),
-          borderRadius:responsiveWidth(10),
-          backgroundColor: 'green',
-          marginLeft:responsiveWidth(5)
-        }}></View>
-        <View
-        style={{
-          width: responsiveWidth(20),
-          height: responsiveWidth(20),
-          borderRadius:responsiveWidth(10),
-          backgroundColor: 'green',
-          marginLeft:responsiveWidth(5)
-        }}></View>
-        <View
-        style={{
-          width: responsiveWidth(20),
-          height: responsiveWidth(20),
-          borderRadius:responsiveWidth(10),
-          backgroundColor: 'green',
-          marginLeft:responsiveWidth(5)
-        }}></View>
-        <View
-        style={{
-          width: responsiveWidth(20),
-          height: responsiveWidth(20),
-          borderRadius:responsiveWidth(10),
-          backgroundColor: 'green',
-          marginLeft:responsiveWidth(5)
-        }}></View>
-        <View
-        style={{
-          width: responsiveWidth(20),
-          height: responsiveWidth(20),
-          borderRadius:responsiveWidth(10),
-          backgroundColor: 'green',
-          marginLeft:responsiveWidth(5)
-        }}></View>
-        </View>
-      <TouchableOpacity
-        style={{
-          width: responsiveWidth(90),
-          height: responsiveHeight(10),
-          backgroundColor: '#000',
-          alignItems: 'center',
-          justifyContent: 'center',
-          borderRadius: responsiveWidth(4),
-        }}>
-        <Text style={{color: '#fff', fontSize: responsiveFontSize(3)}}>
-          Touch Me
-        </Text>
-      </TouchableOpacity>
-      <Image
-        resizeMode="contain"
-        source={require('./SRC/Images/Drawer.png')}
-        style={{width: responsiveWidth(20), height: responsiveHeight(20)}}
+    <View style={{flex: 1}}>
+      <FlatList
+        data={data}
+        renderItem={({item, index}) => {
+          return (
+            <TouchableOpacity
+              style={{
+                width: '100%',
+                height: 70,
+                borderColor: '#8e8e8e',
+                borderWidth: 0.2,
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderWidth: item == true ? 5 : 1,
+                backgroundColor:item==true ? 'blue': 'green',
+                borderColor:item==true ? 'blue' : 'pink'
+              }}
+              onPress={() => {
+                onSelect(index);
+              }}>
+              <Text style={{fontSize: 30, color: 'black', color:item==true ? 'yellow' : 'red'}}>
+                {'item ' + (index + 1)}
+              </Text>
+            </TouchableOpacity>
+          );
+        }}
       />
-      <Text
-        style={{
-          fontSize: responsiveFontSize(2.5),
-          marginTop: responsiveHeight(10),
-        }}>
-        Donate: If you use this site regularly and would like to help keep the
-        site on the Internet, please consider donating a small sum to help pay
-        for the hosting and bandwidth bill. There is no minimum donation, any
-        sum is appreciated - click here to donate using PayPal. Thank you for
-        your support.
-      </Text>
     </View>
   );
 }
