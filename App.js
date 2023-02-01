e2b171f0bdb36a70ee9ab4ad97604a48728c4c7e
Tanveer Tamboli <tanveertamboli80@gmail.com>
2023-02-02 00:19:52 +0530

    AsyncStorage Display Data

--- App.js
+++ App.js
@@ -1,70 +1,86 @@
-import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import react, {useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
-  const [data, setdata] = useState([
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-  ]);
+  const [text, setText] = useState('');
+  const [savedtext, setSavedText] = useState('');
 
-  const onSelect = ind => {
-    const tempData = [];
-    data.map((item, index) => {
-      if (index == ind) {
-        if (item==true){
-          tempData.push(false)
-        } else{
-            tempData.push(true)
-          }
-      } else {
-        if (item==true){
-          tempData.push(true)
-        } else{
-            tempData.push(false)
-          }
+  const storeData = async () => {
+    try {
+      await AsyncStorage.setItem('TEXT', text);
+    } catch (e) {
+      // saving error
+    }
+  };
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('TEXT');
+      if (value !== null) {
+        setSavedText(value);
+        // value previously stored
       }
-    });
-    setdata(tempData);
+    } catch (e) {
+      // error reading value
+    }
   };
 
   return (
-    <View style={{flex: 1}}>
-      <FlatList
-        data={data}
-        renderItem={({item, index}) => {
-          return (
-            <TouchableOpacity
-              style={{
-                width: '100%',
-                height: 70,
-                borderColor: '#8e8e8e',
-                borderWidth: 0.2,
-                justifyContent: 'center',
-                alignItems: 'center',
-                borderWidth: item == true ? 5 : 1,
-                backgroundColor:item==true ? 'blue': 'green',
-                borderColor:item==true ? 'blue' : 'pink'
-              }}
-              onPress={() => {
-                onSelect(index);
-              }}>
-              <Text style={{fontSize: 30, color: 'black', color:item==true ? 'yellow' : 'red'}}>
-                {'item ' + (index + 1)}
-              </Text>
-            </TouchableOpacity>
-          );
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <TextInput
+        onChangeText={txt => setText(txt)}
+        style={{
+          width: '80%',
+          height: 70,
+          backgroundColor: 'green',
+          borderRadius: 13,
+          paddingHorizontal: 20,
         }}
       />
+      <TouchableOpacity
+        onPress={() => {
+          storeData();
+        }}
+        style={{
+          width: 200,
+          height: 70,
+          backgroundColor: '#000',
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: 10,
+          marginTop: 30,
+        }}>
+        <Text style={{color: '#fff'}}>Save Data</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          getData();
+        }}
+        style={{
+          width: 200,
+          height: 70,
+          backgroundColor: '#000',
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: 10,
+          marginTop: 50,
+        }}>
+        <Text style={{color: '#fff'}}>Display Value</Text>
+      </TouchableOpacity>
+      <Text style={{marginTop: 20}}>{'Save Text :' + savedtext}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const style = StyleSheet.create({});
